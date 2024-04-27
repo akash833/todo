2b28d161a3fd1b14f84bdbdc9d615e4e9446a01c
Saurabh Bisht <bishtsaurabh971@gmail.com>
2024-04-27 16:23:27 +0530

    login

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,12 +1,27 @@
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
 import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Login.css";
+
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import GoogleIcon from "@mui/icons-material/Google";
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const navigate = useNavigate();
 
@@ -18,6 +33,7 @@ const LoginPage = () => {
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
+    console.log(event);
     const res = await axios.post(`http://localhost:4000/login`, {
       username,
       password,
@@ -36,36 +52,81 @@ const LoginPage = () => {
   return (
     <div className="loginWrapper">
       <div className="childWrapper">
-        
+        <div className="imageContainer">
+          <img
+            src="https://c8.alamy.com/comp/2BE5RAF/a-girl-working-in-laptopillustration-concept-of-work-from-home-2BE5RAF.jpg"
+            alt=""
+            height="100%"
+            width="100%"
+          />
+        </div>
+        <div className="loginContainer">
+          <h2 className="login">Login!</h2>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Username"
+              variant="outlined"
+              name="username"
+              value={username}
+              onChange={handleInputChange}
+              required
+              className="username"
+            />
+            <br />
+            <br />
+            <FormControl sx={{ width: "100%" }} variant="outlined" required>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleInputChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {!showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <br />
+            <br />
+
+            <div style={{ textAlign: "center" }}>
+              Create a new Account?
+              <span onClick={handleClick}>
+                <Button variant="text">Sign Up</Button>
+              </span>
+            </div>
+            <div className="btnWrapper">
+              <Button
+                sx={{ textAlign: "center", width: "100%" }}
+                type="submit"
+                variant="outlined"
+              >
+                Login
+              </Button>
+              <br />
+              <br />
+              <Button className="googleSignUp" variant="contained">
+                <GoogleIcon sx={{ marginRight: "10px" }} />
+                Continue with Google
+              </Button>
+            </div>
+          </form>
+        </div>
+
+        <ToastContainer />
       </div>
-      <h2>Login Page</h2>
-      <ToastContainer />
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          Create Account <button onClick={handleClick}>Click here</button>
-        </div>
-        <button type="submit">Login</button>
-      </form>
     </div>
   );
 };
--- client/src/Components/SignUp.tsx
+++ client/src/Components/SignUp.tsx
@@ -1,3 +1,13 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -7,7 +17,14 @@ import "react-toastify/dist/ReactToastify.css";
 const SignUpPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowConfirmPassword = () =>
+    setShowConfirmPassword((show) => !show);
 
   const navigate = useNavigate();
 
@@ -16,10 +33,12 @@ const SignUpPage = () => {
     if (name === "username") setUsername(value);
     else if (name === "password") setPassword(value);
     else if (name === "confirmPassword") setConfirmPassword(value);
+    else if (name === "email") setEmail(value);
   };
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
+
     if (password !== confirmPassword) {
       return toast(`Password doesn't match`);
     }
@@ -39,46 +58,122 @@ const SignUpPage = () => {
   };
 
   return (
-    <div>
-      <h2>Signup Page</h2>
-      <ToastContainer />
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={handleInputChange}
-            required
+    <div className="loginWrapper">
+      <div className="childWrapper">
+        <div className="imageContainer">
+          <img
+            src=" https://cdn-lite.ip2location.com/img/sign-up.png "
+            alt=""
+            height="100%"
+            width="100%"
           />
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleInputChange}
-            required
-          />
+        <div className="loginContainer">
+          <h2 className="login">Sign Up!</h2>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Username"
+              variant="outlined"
+              value={username}
+              name="username"
+              onChange={handleInputChange}
+              required
+              className="username"
+            />
+            <br />
+            <br />
+            <TextField
+              label="Email"
+              type="email"
+              variant="outlined"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              required
+              className="username"
+            />
+            <br />
+            <br />
+
+            <FormControl sx={{ width: "100%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                name="password"
+                onChange={handleInputChange}
+                required
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {!showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+
+            <br />
+            <br />
+
+            <FormControl sx={{ width: "100%" }} variant="outlined" required>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Confirm Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showConfirmPassword ? "text" : "password"}
+                value={confirmPassword}
+                name="confirmPassword"
+                onChange={handleInputChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowConfirmPassword}
+                      edge="end"
+                    >
+                      {!showConfirmPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <br />
+            <br />
+            <div style={{ textAlign: "center" }}>
+              Already have an Account?
+              <span onClick={handleClick}>
+                <Button variant="text">Login</Button>
+              </span>
+            </div>
+            <div className="btnWrapper">
+              <Button
+                sx={{ textAlign: "center", width: "100%" }}
+                type="submit"
+                variant="outlined"
+              >
+                Sign up
+              </Button>
+            </div>
+          </form>
         </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          Already have an account ?{" "}
-          <button onClick={handleClick}>Click here</button>
-        </div>
-        <button type="submit">Signup</button>
-      </form>
+
+        <ToastContainer />
+      </div>
     </div>
   );
 };
